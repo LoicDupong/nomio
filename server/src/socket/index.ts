a83ffabaaba421851e95c00020ddb62fe737e940
LoicDupong <loic@dupong.be>
2026-04-16 17:01:43 +0200

    fix: type socket stub properly, remove any return type

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,3 +1,13 @@
 import http from 'http';
-export function initSocket(_server: http.Server) {}
-export function getIO(): any { throw new Error('Socket not initialized'); }
+import { Server } from 'socket.io';
+
+let io: Server | undefined;
+
+export function initSocket(_server: http.Server) {
+  // Will be implemented in Task 8
+}
+
+export function getIO(): Server {
+  if (!io) throw new Error('Socket not initialized');
+  return io;
+}
