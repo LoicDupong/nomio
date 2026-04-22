0328f731d0fc5c6a83d5e1bb330eaf40e4ea76bf
LoicDupong <loic@dupong.be>
2026-04-22 18:52:43 +0200

    SSL Fix

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,11 +3,6 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: 'postgres',
   logging: false,
-  dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production'
-      ? { require: true, rejectUnauthorized: false }
-      : false,
-  },
 });
 
 import { User } from './User';
