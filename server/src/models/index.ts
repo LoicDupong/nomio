54166ea20b67a402050aa20e3947969b59ea6020
LoicDupong <loic@dupong.be>
2026-04-22 20:21:35 +0200

    DB URL debugging tests

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -31,12 +31,9 @@ try {
 //   },
 // });
 
-export const sequelize = new Sequelize(dbUrl, {
+export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: 'postgres',
   logging: false,
-  dialectOptions: {
-    ssl: { require: true, rejectUnauthorized: false },
-  },
 });
 
 import { User } from './User';
